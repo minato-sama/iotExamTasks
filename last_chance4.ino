33d75389a0bce57b8be3810a6db697a781d167ce
Сухарев ! <71521257+minato-sama@users.noreply.github.com>
2021-12-28 21:51:32 +0800

    Add files via upload

---
+++ last_chance4.ino
@@ -0,0 +1,32 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11);
+void setup() {
+  pinMode(10, INPUT);
+  pinMode(11, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  Serial.begin(1000);
+  mySerial.begin(19200);
+  delay(50);
+}
+
+
+void loop() {
+  if (Serial.available() >= 3) {
+
+    int16_t red6 = Serial.read();
+    int16_t green3 = Serial.read();
+    int16_t blue5 = Serial.read();
+    
+    analogWrite(3, green3);
+    analogWrite(5, blue5);
+    analogWrite(6, red6);
+
+    while (Serial.available() > 0) {
+      mySerial.write(Serial.read());
+    } 
+  }
+  delay(100);
+}
